fdce1d45950d6dd2739f9218424e7cdebd4000a4
DkDeepak001 <deepakeswar001@gmail.com>
2022-07-23 18:44:19 +0530

    add jst authentication

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const ejs  = require("ejs");
 const app = express();
 const reload = require('reload');
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const cookieParser = require("cookie-parser");
+
+require("dotenv").config();
 
 
 //set view engine
@@ -13,6 +17,7 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cookieParser());
 
 //connect mongoose 
 mongoose.connect("mongodb://localhost:27017/Login_auth");
@@ -26,6 +31,11 @@ const loginSchema = new mongoose.Schema({
 
 const User = new mongoose.model("User_Login_Data",loginSchema);
 
+const superUserSchemea = new mongoose.Schema({
+    superPower:loginSchema,
+    role:String
+})
+const superUser = new mongoose.model("superUser", superUserSchemea);
 
 
 
@@ -43,10 +53,19 @@ app.route("/login")
             if(user){
                  bcrypt.compare(formPassowrd, user.password,async(err, result) => {
                     const foundResult = await result;
-                    if(foundResult === true){
-                        res.redirect("/");
-                    }else{
-                        res.send("incorrect password");
+                    try{
+                        if(foundResult === true){
+                            jwt.sign({ AuthId: user._id }, process.env.JWT_SECRET_KEY, function(err, token) {
+                                res.cookie("AuthToken",token);
+                                res.redirect("/");
+                            });
+                            
+                        }else{
+                            res.send("incorrect password");
+                        }
+                    }
+                    catch(e){
+                        res.send(e);
                     }
                 });            
             }else{
@@ -90,16 +109,57 @@ app.route("/register")
        }
     })
 
+//function to check token is valid
+
+function TokenValidator(req,res,next){
+    if(req.cookies.AuthToken){
+         jwt.verify(req.cookies.AuthToken, process.env.JWT_SECRET_KEY , async(err, decoded) => {
+            const userID = await(decoded.AuthId);
+            User.findById(userID, (err,userDetails) =>{
+                if(err){
+                    alert("some error with checking your id with database!, please try again")
+                    res.clearCookie('AuthToken');
+                    res.redirect("/login");
+                    return;
+                }else{
+                    if(userID === userDetails.id){
+                        console.log("true")
+                        next();
+                    }else{
+                        alert("some error with checking your id with database!, please try again")
+                        res.clearCookie('AuthToken');
+                        res.redirect("/login");
+                        return;
+                    }
+                }
+            } )
+          });
+    }else{
+        res.redirect("/login");
+    }
+}
+
+
 //route config for Home page
 app.route("/")
-    .get((req,res) => {
+    .get(TokenValidator,(req,res) => {
         res.render("home");
+
     })
 
 //route config for auth page
 app.route("/auth")
     .get((req,res) => {
         res.render("auth");
+    })
+
+//route config for addAdmin page
+app.route("/addAdmin")
+    .get(TokenValidator,(req,res) => {
+        res.render("addAdmin");
+    })
+    .post((req,res) => {
+       console.log(req.body.username);
     })
 
 
